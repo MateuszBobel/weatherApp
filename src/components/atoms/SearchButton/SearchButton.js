008433925d8d2f3ab6e483158154d9d4c1a8bd48
Mateusz Bobel <mateusz.bobel.mb@gmail.com>
2020-05-17 11:50:12 +0200

    create search button

---
+++ src/components/atoms/SearchButton/SearchButton.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './SearchButton.module.css';
+
+const SearchButton = () => <button className={styles.searchBtn}>Search</button>;
+
+export default SearchButton;
