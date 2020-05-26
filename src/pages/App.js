9b7f4a61ab8ab27926bcde675a0397765ec94693
Mateusz Bobel <mateusz.bobel.mb@gmail.com>
2020-05-26 08:29:09 +0200

    add dots loader

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all';
+import Loader from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import styles from './App.module.css';
-import SearchInput from '../components/atoms/SearchInput/SearchInput';
+import MainPanel from '../components/organisms/MainPanel/MainPanel';
+import HighlightsPanel from '../components/organisms/HighlightsPanel/HighlightsPanel';
 
 function App() {
+  useEffect(() => {
+    setStateValue({
+      ...stateValue,
+      isLoaded: true,
+    });
+  }, []);
+
+  const [stateValue, setStateValue] = useState({
+    inputValue: '',
+    isError: false,
+    isLoaded: false,
+
+    currentWeather: {
+      location: '',
+      description: '',
+      cloudiness: '',
+      mainTemperature: 0,
+      windSpeed: 0,
+    },
+
+    highlightParameters: {
+      minTemp: 0,
+      maxTemp: 0,
+      sunrise: 0,
+      sunset: 0,
+      visibility: 0,
+      humidity: 0,
+      pressure: 0,
+    },
+  });
+
   return (
     <div className={styles.App}>
-      <SearchInput />
+      {stateValue.isLoaded ? (
+        <Loader
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}
+          type='ThreeDots'
+          color='#000'
+          height={100}
+          width={100}
+          timeout={5000}
+        />
+      ) : (
+        <>
+          <MainPanel stateValue={stateValue} setStateValue={setStateValue} />
+          <HighlightsPanel stateValue={stateValue.highlightParameters} />
+        </>
+      )}
     </div>
   );
 }
