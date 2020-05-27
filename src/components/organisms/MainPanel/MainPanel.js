b6c71861e287472d285ec14066cb658812f05f1c
Mateusz Bobel <mateusz.bobel.mb@gmail.com>
2020-05-27 12:34:52 +0200

    change API address in MainPanel

--- src/components/organisms/MainPanel/MainPanel.js
+++ src/components/organisms/MainPanel/MainPanel.js
@@ -8,7 +8,7 @@ import MainInfoPanel from '../../molecules/MainInfoPanel/MainInfoPanel';
 
 const MainPanel = ({ stateValue, setStateValue }) => {
   useEffect(() => {
-    const url = 'http://ip-api.com/json/';
+    const url = 'https://ipapi.co/8.8.8.8/json/';
 
     fetch(url)
       .then((response) => {
